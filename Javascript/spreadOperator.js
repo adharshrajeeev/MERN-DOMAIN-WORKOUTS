1ed33bfd7c9b04322f4d98ea6f02d8e1c30cf1c4
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-01 13:49:32 +0530

    mongodb and javascript spread operator added

---
+++ Javascript/spreadOperator.js
@@ -0,0 +1,16 @@
+
+
+var arr1=[1,2,3,4,5,6];
+var arr2=[7,8,9,10];
+
+console.log([...arr1,...arr2])
+
+
+var obj1={
+    name:"salmon",
+    age:"11"
+};
+
+const obj2={...obj1};
+
+console.log(obj2)
