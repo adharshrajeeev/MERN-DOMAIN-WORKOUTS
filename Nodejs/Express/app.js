d26bfbcad475b81269400af173e77f8ad358fb74
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-03 18:14:14 +0530

    added redux and explained prop driling with illsutration

--- Nodejs/Express/app.js
+++ Nodejs/Express/app.js
@@ -7,17 +7,23 @@ const PORT=process.env.ServerPort;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-const token="123";
+app.use(function(err,req,res,next){
+    res.status(500).json("SOMETHING WENT WROING")
 
-function Token(req,res,next){
-    console.log("Creating token")
-    next();
-}
+})
+// function Token(req,res,next){
+//     console.log("Creating token")
+//     next();
+// }
 
-app.get("/profile",Token,(req,res)=>{
-    console.log("USER LOGGED")
+// app.get("/profile",Token,(req,res)=>{
+//     console.log("USER LOGGED")
 
-    res.send("<h1>Sucess</h1>")
+//     res.send("<h1>Sucess</h1>")
+// })
+
+app.get('/',(req,res)=>{
+    res.send("HELLO IAM HERE")
 })
 
 
