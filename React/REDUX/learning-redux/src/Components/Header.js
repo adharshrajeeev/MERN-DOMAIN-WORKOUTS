4b125b7c49264e94c2176a7f5649dd197f5fcdad
Adharsh R <adharshrajeeev2000@gmail.com>
2023-03-03 22:55:43 +0530

    added counter in header using redux

--- React/REDUX/learning-redux/src/Components/Header.js
+++ React/REDUX/learning-redux/src/Components/Header.js
@@ -3,10 +3,11 @@ import { useSelector } from "react-redux";
 
 function Header() {
   const color=useSelector(state=>state.color.value)
+  const count=useSelector(state=>state.count.value)
   return (
     <div className="header">
       <h1 style={{color}}>REDUX Project</h1>
-      <h3> </h3>
+      <h3>{count} </h3>
     </div>
   );
 }
